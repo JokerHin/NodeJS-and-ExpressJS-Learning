49387150f053bc74db67b26b3b2efd8c516ef099
JokerHin <chokarhin@gmail.com>
2025-01-25 19:15:35 +0800

    update

--- app.js
+++ app.js
@@ -3,9 +3,8 @@
 const names = require("./4-name.js");
 const sayHi = require("./5-utils.js");
 const data = require("./6-alternative-flavor.js");
+require("./7-mind-grenade.js");
 
-console.log(data);
-
-sayHi("susan");
-sayHi(names.john);
-sayHi(names.peter);
+// sayHi("susan");
+// sayHi(names.john);
+// sayHi(names.peter);
