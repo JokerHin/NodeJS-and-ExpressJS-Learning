b60bc9f7466c2f198500c3156b8a63d8f8ed72a6
JokerHin <chokarhin@gmail.com>
2025-01-25 19:11:12 +0800

    update

--- app.js
+++ app.js
@@ -2,8 +2,9 @@
 // Modules - Encapsulated Code (only share mininum)
 const names = require("./4-name.js");
 const sayHi = require("./5-utils.js");
-console.log(names);
-console.log(sayHi);
+const data = require("./6-alternative-flavor.js");
+
+console.log(data);
 
 sayHi("susan");
 sayHi(names.john);
