8869b5c795d491cb7d35d1b069a70dd4f2b18b83
JokerHin <chokarhin@gmail.com>
2025-01-25 18:31:03 +0800

    update

---
+++ app.js
@@ -0,0 +1,7 @@
+//CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share mininum)
+const names = require("./4-names");
+
+sayHi("susan");
+sayHi(john);
+sayHi(peter);
