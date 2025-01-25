02686ff465e9dbcd3e183d701a808f80b03cff5e
JokerHin <chokarhin@gmail.com>
2025-01-25 19:30:05 +0800

    update

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+//CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share mininum)
+const names = require("./4-name.js");
+const sayHi = require("./5-utils.js");
+const data = require("./6-alternative-flavor.js");
+require("./7-mind-grenade.js");
+
+// sayHi("susan");
+// sayHi(names.john);
+// sayHi(names.peter);
--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-//CommonJS, every file is module (by default)
-// Modules - Encapsulated Code (only share mininum)
-const names = require("./4-name.js");
-const sayHi = require("./5-utils.js");
-const data = require("./6-alternative-flavor.js");
-require("./7-mind-grenade.js");
+const os = require("path");
 
-// sayHi("susan");
-// sayHi(names.john);
-// sayHi(names.peter);
+console.log(path.sep);
