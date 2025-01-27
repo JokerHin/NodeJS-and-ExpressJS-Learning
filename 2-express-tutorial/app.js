b0ac920118e95f57b6241e68b9d9748cbe23c683
JokerHin <chokarhin@gmail.com>
2025-01-27 23:38:42 +0800

    update

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -4,9 +4,60 @@ const app = express();
 const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send('<h1>Home Page</h1><a href="/api/products">products');
 });
 
-app.listen(5000, (req, res) => {
+// using map to get only id, name and image from the json data
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+// use req,params to get the productID from the url and find the product with that id,
+// so no need to set the id url one by one
+// the req.params will show in string so we need to convert it to number
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  // if the singleProduct is undefined, return 404 status and send the message
+  if (!singleProduct) {
+    return res.status(404).send("Product Does Not Exist");
+  }
+  res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
+});
+
+app.get("api/v1/query", (req, res) => {
+  // console.log(req, query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  // everything get from req.query is string so we need to convert it to number
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    res.status(200).send("no products matched your search");
+  }
+  res.status(200).json(sortedProducts);
+});
+
+app.listen(5000, () => {
   console.log("Server is listening on port 5000");
 });
