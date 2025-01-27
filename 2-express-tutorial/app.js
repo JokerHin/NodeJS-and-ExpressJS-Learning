e578b26001d8108fe66eb1572d8d3447c3e7391e
JokerHin <chokarhin@gmail.com>
2025-01-27 22:28:28 +0800

    update

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -7,10 +7,12 @@ const app = express();
 //serves files (e.g., images, CSS, JavaScript) directly from the “public” folder so users can access them through HTTP requests without additional routing.
 app.use(express.static("./public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-  // It sends the specified “index.html” file as the HTTP response, making sure the file path is resolved correctly based on the server's environment.
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+//  // It sends the specified “index.html” file as the HTTP response, making sure the file path is resolved correctly based on the server's environment.
+//  adding to static assets
+//  SSR
+// });
 
 app.all("*", (req, res) => {
   res.status(404).send("resource not found");
