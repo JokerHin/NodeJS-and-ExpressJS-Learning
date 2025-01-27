5ca530cb5f093cc2d4878b28afb592f4e2b89dcf
JokerHin <chokarhin@gmail.com>
2025-01-27 17:16:47 +0800

    update

--- 2-express-tutorial/final/03-express-basics.js
+++ 2-express-tutorial/final/03-express-basics.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
-})
+// if the url is /, then the callback function will be executed
+// res.send = res.write + res.end
+app.get("/", (req, res) => {
+  console.log("user hit the resource");
+  res.status(200).send("Home page");
+});
 
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
-})
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
-})
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
-})
+  console.log("server is listening on port 5000");
+});
 
 // app.get
 // app.post
