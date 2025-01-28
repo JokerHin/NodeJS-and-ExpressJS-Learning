423f4add4e235accec75dfb10d06a437eed0b22f
JokerHin <chokarhin@gmail.com>
2025-01-28 16:13:14 +0800

    update

--- 2-express-tutorial/final/07-params-query.js
+++ 2-express-tutorial/final/07-params-query.js
@@ -1,58 +1,63 @@
-const express = require('express')
-const app = express()
-const { products } = require('./data')
+// set the json middleware and send json data
+const express = require("express");
+const app = express();
+const { products } = require("./data");
 
-app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
-})
-app.get('/api/products', (req, res) => {
+app.get("/", (req, res) => {
+  res.send('<h1>Home Page</h1><a href="/api/products">products');
+});
+
+// using map to get only id, name and image from the json data
+app.get("/api/products", (req, res) => {
   const newProducts = products.map((product) => {
-    const { id, name, image } = product
-    return { id, name, image }
-  })
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
 
-  res.json(newProducts)
-})
-app.get('/api/products/:productID', (req, res) => {
-  // console.log(req)
-  // console.log(req.params)
-  const { productID } = req.params
-
+// use req,params to get the productID from the url and find the product with that id,
+// so no need to set the id url one by one
+// the req.params will show in string so we need to convert it to number
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  const { productID } = req.params;
   const singleProduct = products.find(
     (product) => product.id === Number(productID)
-  )
+  );
+  // if the singleProduct is undefined, return 404 status and send the message
   if (!singleProduct) {
-    return res.status(404).send('Product Does Not Exist')
+    return res.status(404).send("Product Does Not Exist");
   }
+  res.json(singleProduct);
+});
 
-  return res.json(singleProduct)
-})
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
+});
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
-})
-
-app.get('/api/v1/query', (req, res) => {
-  // console.log(req.query)
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
+app.get("api/v1/query", (req, res) => {
+  // console.log(req, query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
 
   if (search) {
     sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search)
-    })
+      return product.name.startsWith(search);
+    });
   }
+  // everything get from req.query is string so we need to convert it to number
   if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
+    sortedProducts = sortedProducts.slice(0, Number(limit));
   }
   if (sortedProducts.length < 1) {
-    // res.status(200).send('no products matched your search');
-    return res.status(200).json({ sucess: true, data: [] })
+    res.status(200).send("no products matched your search");
   }
-  res.status(200).json(sortedProducts)
-})
+  res.status(200).json(sortedProducts);
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("Server is listening on port 5000");
+});
