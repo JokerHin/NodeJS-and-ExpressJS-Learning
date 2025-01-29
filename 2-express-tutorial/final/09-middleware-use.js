7b1cea464d90ef17ea223ccb2837a0047c684e1b
JokerHin <chokarhin@gmail.com>
2025-01-29 14:38:18 +0800

    update

--- 2-express-tutorial/final/09-middleware-use.js
+++ 2-express-tutorial/final/09-middleware-use.js
@@ -1,24 +1,30 @@
-const express = require('express')
-const app = express()
-const logger = require('./logger')
-const authorize = require('./authorize')
-//  req => middleware => res
-app.use([logger, authorize])
+// middleware is a function during request to the server
+const express = require("express");
+const app = express();
+const logger = require("./logger.js");
+const authorize = require("./authorize.js");
+
+// req => middleware => res
+
+// 1. use vs route
+// 2. options - our own / express /third party
+
+app.use([logger, authorize]); //middleware
 // api/home/about/products
-app.get('/', (req, res) => {
-  res.send('Home')
-})
-app.get('/about', (req, res) => {
-  res.send('About')
-})
-app.get('/api/products', (req, res) => {
-  res.send('Products')
-})
-app.get('/api/items', (req, res) => {
-  console.log(req.user)
-  res.send('Items')
-})
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.get("/api/products", (req, res) => {
+  res.send("Products");
+});
+app.get("/api/items", (req, res) => {
+  console.log(req.user);
+  res.send("Items");
+});
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
-})
+  console.log("Server is listening on port 5000....");
+});
