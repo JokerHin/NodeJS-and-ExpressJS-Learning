a09511ae129248d7a2f13c1fe227fbaabbed948c
JokerHin <chokarhin@gmail.com>
2025-01-29 13:47:44 +0800

    update

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,63 +1,24 @@
-// set the json middleware and send json data
+// middleware is a function during request to the server
 const express = require("express");
 const app = express();
-const { products } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">products');
+// req => middleware => res
+
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
+
+app.get("/", logger, (req, res) => {
+  res.status(200).send("Home");
 });
-
-// using map to get only id, name and image from the json data
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
-});
-
-// use req,params to get the productID from the url and find the product with that id,
-// so no need to set the id url one by one
-// the req.params will show in string so we need to convert it to number
-app.get("/api/products/:productID", (req, res) => {
-  // console.log(req);
-  // console.log(req.params);
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  // if the singleProduct is undefined, return 404 status and send the message
-  if (!singleProduct) {
-    return res.status(404).send("Product Does Not Exist");
-  }
-  res.json(singleProduct);
-});
-
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("hello world");
-});
-
-app.get("api/v1/query", (req, res) => {
-  // console.log(req, query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  // everything get from req.query is string so we need to convert it to number
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    res.status(200).send("no products matched your search");
-  }
-  res.status(200).json(sortedProducts);
+app.get("/about", logger, (req, res) => {
+  res.status(200).send("About");
 });
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000");
+  console.log("Server is listening on port 5000....");
 });
