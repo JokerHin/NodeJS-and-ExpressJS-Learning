a00fb8c79813d83ac186c20c31eb57f1496d3484
JokerHin <chokarhin@gmail.com>
2025-01-30 13:17:26 +0800

    update

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,33 +1,71 @@
-// middleware is a function during request to the server
+// http method
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const logger = require("./logger.js");
-const authorize = require("./authorize.js");
+const { people } = require("./data");
 
-// req => middleware => res
+// static assets
+app.use(express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+//parse json
+app.use(express.json());
 
-// 1. use vs route
-// 2. options - our own / express /third party
+//Get Method
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
 
-// app.use([logger, authorize]); //middleware
-// api/home/about/products
-// app.use(express.static("./public"));
-app.use(morgan("tiny"));
-app.get("/", (req, res) => {
-  res.send("Home");
+//Insert data
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+  res.status(201).send({ success: true, person: name });
 });
-app.get("/about", (req, res) => {
-  res.send("About");
+
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide name value" });
+  }
+  res.status(201).send({ success: true, person: [...people, name] });
 });
-app.get("/api/products", (req, res) => {
-  res.send("Products");
+
+app.post("/login", (req, res) => {
+  // console.log(req.body);
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please Provide Credentials");
 });
-app.get("/api/items", [logger, authorize], (req, res) => {
-  console.log(req.user);
-  res.send("Items");
+
+// update
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000....");
+  console.log("Server is listening on port 5000...");
 });
