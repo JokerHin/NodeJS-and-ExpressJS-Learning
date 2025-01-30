4cf6e0646e816bdaacf1da1884bf8ea67e5a6253
JokerHin <chokarhin@gmail.com>
2025-01-30 15:45:16 +0800

    update

--- 1-node-tutorial/10-fs-sync.js
+++ 1-node-tutorial/10-fs-sync.js
@@ -7,7 +7,7 @@ const second = readFileSync("./content/second.txt", "utf8");
 writeFileSync(
   "./content/result-sync.txt",
   `Here is the result : ${first}, ${second}`,
-  { flag: "a" }
+  { flag: "a" } //append
 );
 console.log("done with this task");
 console.log("stating the next one");
--- 1-node-tutorial/7-mind-grenade.js
+++ 1-node-tutorial/7-mind-grenade.js
@@ -5,4 +5,5 @@ function addValues() {
   console.log(`the sum is: ${num1 + num2}`);
 }
 
+// call the function
 addValues();
